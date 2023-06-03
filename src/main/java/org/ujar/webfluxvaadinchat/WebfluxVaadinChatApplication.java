6199d5bf123e557364bc6aa4a7469a5f2331172e
Dima Denysenko <coding@dimdnk.com>
2023-06-03 18:36:51 +0300

    chore: enable reasonable defaults for observability components

--- src/main/java/org/ujar/webfluxvaadinchat/WebfluxVaadinChatApplication.java
+++ src/main/java/org/ujar/webfluxvaadinchat/WebfluxVaadinChatApplication.java
@@ -4,12 +4,15 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 @Push
 public class WebfluxVaadinChatApplication implements AppShellConfigurator {
   public static void main(String[] args) {
-    SpringApplication.run(WebfluxVaadinChatApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(WebfluxVaadinChatApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
 
